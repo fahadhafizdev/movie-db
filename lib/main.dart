8f6c3a7dbe3f8b842bf813c44d7112551b2fb16f
fahadhafiz dev <fahadhafiz.dev@gmail.com>
2021-11-16 18:21:31 +0700

    Update main.dart
    add provider

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,9 @@ import 'package:flutter/material.dart';
 import 'package:moviez_streaming_dark/pages/splash_page.dart';
 import 'package:moviez_streaming_dark/providers/category_menu_provider.dart';
 import 'package:moviez_streaming_dark/providers/movie_provider.dart';
+import 'package:moviez_streaming_dark/providers/nowplaying_movie_provider.dart';
 import 'package:moviez_streaming_dark/providers/popular_movie_provider.dart';
+import 'package:moviez_streaming_dark/providers/upcoming_movie_provider.dart';
 import 'pages/home/home_page.dart';
 import 'package:provider/provider.dart';
 
@@ -18,6 +20,8 @@ class MyApp extends StatelessWidget {
         ChangeNotifierProvider(create: (context) => MovieProvider()),
         ChangeNotifierProvider(create: (context) => PopularMovieProvider()),
         ChangeNotifierProvider(create: (context) => CategoryMenuProvider()),
+        ChangeNotifierProvider(create: (context) => NowPlayingMovieProvider()),
+        ChangeNotifierProvider(create: (context) => UpComingMovieProvider()),
       ],
       child: MaterialApp(
         debugShowCheckedModeBanner: false,
