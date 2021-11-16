b81929974075a6bda9fed53539998ca1d86eb658
fahadhafiz dev <fahadhafiz.dev@gmail.com>
2021-11-17 01:28:17 +0700

    Update splash_page.dart

--- lib/pages/splash_page.dart
+++ lib/pages/splash_page.dart
@@ -5,6 +5,9 @@ import 'package:moviez_streaming_dark/providers/movie_provider.dart';
 import 'package:moviez_streaming_dark/providers/nowplaying_movie_provider.dart';
 import 'package:moviez_streaming_dark/providers/popular_movie_provider.dart';
 import 'package:moviez_streaming_dark/providers/tv_airingtoday_provider.dart';
+import 'package:moviez_streaming_dark/providers/tv_ontheair_provider.dart';
+import 'package:moviez_streaming_dark/providers/tv_popular_provider.dart';
+import 'package:moviez_streaming_dark/providers/tv_toprated_provider.dart';
 import 'package:moviez_streaming_dark/providers/upcoming_movie_provider.dart';
 import 'package:moviez_streaming_dark/theme.dart';
 import 'package:provider/provider.dart';
@@ -34,6 +37,12 @@ class _SplashPageState extends State<SplashPage> {
           .getUpComingMovieList();
       await Provider.of<TvAiringTodayProvider>(context, listen: false)
           .getTvAiringToday();
+      await Provider.of<TvTopRatedProvider>(context, listen: false)
+          .getTvTopRated();
+      await Provider.of<TvOnTheAirProvider>(context, listen: false)
+          .getTvOnTheAir();
+      await Provider.of<TvPopularProvider>(context, listen: false)
+          .getTvPopular();
 
       Navigator.pushNamedAndRemoveUntil(
           context, '/main-page', (route) => false);
