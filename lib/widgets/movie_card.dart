6bc2b56e91634997359a12154a9d453e1ec493da
fahadhafiz dev <fahadhafiz.dev@gmail.com>
2021-11-17 13:48:50 +0700

    add route to detail page

--- lib/widgets/movie_card.dart
+++ lib/widgets/movie_card.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:moviez_streaming_dark/models/movie_model.dart';
+import 'package:moviez_streaming_dark/pages/detail_movie_page.dart';
 import 'package:moviez_streaming_dark/theme.dart';
 
 class MovieCard extends StatelessWidget {
@@ -11,80 +12,90 @@ class MovieCard extends StatelessWidget {
   Widget build(BuildContext context) {
     int rating = (movies.voteAverage).round();
 
-    return Container(
-      width: 300,
-      margin: EdgeInsets.only(
-        left: defaultMargin,
-      ),
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          ClipRRect(
-            borderRadius: BorderRadius.circular(21),
-            child: Image.network(
-              'https://www.themoviedb.org/t/p/w533_and_h300_bestv2' +
-                  movies.backDropPath,
-              width: 300,
-              fit: BoxFit.cover,
+    return GestureDetector(
+      onTap: () {
+        Navigator.push(
+          context,
+          MaterialPageRoute(
+            builder: (context) => DetailMoviePage(),
+          ),
+        );
+      },
+      child: Container(
+        width: 300,
+        margin: EdgeInsets.only(
+          left: defaultMargin,
+        ),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            ClipRRect(
+              borderRadius: BorderRadius.circular(21),
+              child: Image.network(
+                'https://www.themoviedb.org/t/p/w533_and_h300_bestv2' +
+                    movies.backDropPath,
+                width: 300,
+                fit: BoxFit.cover,
+              ),
             ),
-          ),
-          SizedBox(
-            height: 20,
-          ),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: [
-              Expanded(
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
+            SizedBox(
+              height: 20,
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Expanded(
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        movies.originalTitle,
+                        style: whiteTextStyle.copyWith(
+                          fontSize: 20,
+                          fontWeight: bold,
+                        ),
+                        overflow: TextOverflow.ellipsis,
+                      ),
+                      SizedBox(
+                        height: 4,
+                      ),
+                      Text(
+                        movies.releaseDate,
+                        style: greyTextStyle.copyWith(
+                          fontSize: 16,
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+                Row(
                   children: [
-                    Text(
-                      movies.originalTitle,
-                      style: whiteTextStyle.copyWith(
-                        fontSize: 20,
-                        fontWeight: bold,
-                      ),
-                      overflow: TextOverflow.ellipsis,
+                    Icon(
+                      Icons.star,
+                      color: rating / 2 >= 1 ? kYellowColor : kGreyColor,
                     ),
-                    SizedBox(
-                      height: 4,
+                    Icon(
+                      Icons.star,
+                      color: rating / 2 >= 2 ? kYellowColor : kGreyColor,
                     ),
-                    Text(
-                      movies.releaseDate,
-                      style: greyTextStyle.copyWith(
-                        fontSize: 16,
-                      ),
+                    Icon(
+                      Icons.star,
+                      color: rating / 2 >= 3 ? kYellowColor : kGreyColor,
+                    ),
+                    Icon(
+                      Icons.star,
+                      color: rating / 2 >= 4 ? kYellowColor : kGreyColor,
+                    ),
+                    Icon(
+                      Icons.star,
+                      color: rating / 2 >= 5 ? kYellowColor : kGreyColor,
                     ),
                   ],
                 ),
-              ),
-              Row(
-                children: [
-                  Icon(
-                    Icons.star,
-                    color: rating / 2 >= 1 ? kYellowColor : kGreyColor,
-                  ),
-                  Icon(
-                    Icons.star,
-                    color: rating / 2 >= 2 ? kYellowColor : kGreyColor,
-                  ),
-                  Icon(
-                    Icons.star,
-                    color: rating / 2 >= 3 ? kYellowColor : kGreyColor,
-                  ),
-                  Icon(
-                    Icons.star,
-                    color: rating / 2 >= 4 ? kYellowColor : kGreyColor,
-                  ),
-                  Icon(
-                    Icons.star,
-                    color: rating / 2 >= 5 ? kYellowColor : kGreyColor,
-                  ),
-                ],
-              ),
-            ],
-          ),
-        ],
+              ],
+            ),
+          ],
+        ),
       ),
     );
   }
--- lib/widgets/movie_tile.dart
+++ lib/widgets/movie_tile.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:moviez_streaming_dark/models/movie_model.dart';
+import 'package:moviez_streaming_dark/pages/detail_movie_page.dart';
 import 'package:moviez_streaming_dark/theme.dart';
 
 class MovieTile extends StatelessWidget {
@@ -11,78 +12,88 @@ class MovieTile extends StatelessWidget {
   Widget build(BuildContext context) {
     int rating = (movies.voteAverage).round();
 
-    return Container(
-      margin: EdgeInsets.only(bottom: 30),
-      child: Row(
-        children: [
-          Container(
-            width: 100,
-            height: 127,
-            margin: EdgeInsets.only(right: 20),
-            decoration: BoxDecoration(
-              borderRadius: BorderRadius.circular(21),
-              image: DecorationImage(
-                fit: BoxFit.cover,
-                image: NetworkImage(
-                  'https://www.themoviedb.org/t/p/w600_and_h900_bestv2' +
-                      movies.posterPath,
+    return GestureDetector(
+      onTap: () {
+        Navigator.push(
+          context,
+          MaterialPageRoute(
+            builder: (context) => DetailMoviePage(),
+          ),
+        );
+      },
+      child: Container(
+        margin: EdgeInsets.only(bottom: 30),
+        child: Row(
+          children: [
+            Container(
+              width: 100,
+              height: 127,
+              margin: EdgeInsets.only(right: 20),
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(21),
+                image: DecorationImage(
+                  fit: BoxFit.cover,
+                  image: NetworkImage(
+                    'https://www.themoviedb.org/t/p/w600_and_h900_bestv2' +
+                        movies.posterPath,
+                  ),
                 ),
               ),
             ),
-          ),
-          Expanded(
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Text(
-                  movies.originalTitle,
-                  style: whiteTextStyle.copyWith(
-                    fontSize: 20,
-                    fontWeight: bold,
+            Expanded(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    movies.originalTitle,
+                    style: whiteTextStyle.copyWith(
+                      fontSize: 20,
+                      fontWeight: bold,
+                    ),
+                    overflow: TextOverflow.clip,
                   ),
-                  overflow: TextOverflow.clip,
-                ),
-                SizedBox(
-                  height: 4,
-                ),
-                Text(
-                  movies.releaseDate,
-                  style: greyTextStyle.copyWith(
-                    fontSize: 16,
+                  SizedBox(
+                    height: 4,
                   ),
-                ),
-                SizedBox(
-                  height: 20,
-                ),
-                Row(
-                  children: [
-                    Icon(
-                      Icons.star,
-                      color: rating / 2 >= 1 ? kYellowColor : kGreyColor,
+                  Text(
+                    movies.releaseDate,
+                    style: greyTextStyle.copyWith(
+                      fontSize: 16,
                     ),
-                    Icon(
-                      Icons.star,
-                      color: rating / 2 >= 2 ? kYellowColor : kGreyColor,
-                    ),
-                    Icon(
-                      Icons.star,
-                      color: rating / 2 >= 3 ? kYellowColor : kGreyColor,
-                    ),
-                    Icon(
-                      Icons.star,
-                      color: rating / 2 >= 4 ? kYellowColor : kGreyColor,
-                    ),
-                    Icon(
-                      Icons.star,
-                      color: rating / 2 >= 5 ? kYellowColor : kGreyColor,
-                    ),
-                  ],
-                ),
-              ],
+                  ),
+                  SizedBox(
+                    height: 20,
+                  ),
+                  Row(
+                    children: [
+                      Icon(
+                        Icons.star,
+                        color: rating / 2 >= 1 ? kYellowColor : kGreyColor,
+                      ),
+                      Icon(
+                        Icons.star,
+                        color: rating / 2 >= 2 ? kYellowColor : kGreyColor,
+                      ),
+                      Icon(
+                        Icons.star,
+                        color: rating / 2 >= 3 ? kYellowColor : kGreyColor,
+                      ),
+                      Icon(
+                        Icons.star,
+                        color: rating / 2 >= 4 ? kYellowColor : kGreyColor,
+                      ),
+                      Icon(
+                        Icons.star,
+                        color: rating / 2 >= 5 ? kYellowColor : kGreyColor,
+                      ),
+                    ],
+                  ),
+                ],
+              ),
             ),
-          ),
-          SizedBox(width: 12),
-        ],
+            SizedBox(width: 12),
+          ],
+        ),
       ),
     );
   }
